db9ca6679027e22aa9cb8319c88f5d8fa5977a44
mondalshefali1982-create <mondalshefali1982@gmail.com>
2026-07-23 16:01:50 +0530

    Delete api directory

--- api/index.ts
+++
@@ -1,8 +0,0 @@
-import app from '../backend/src/app';
-import { connectDB } from '../backend/src/config/database';
-import { Request, Response } from 'express';
-
-export default async (req: Request, res: Response) => {
-  await connectDB();
-  return app(req, res);
-};
